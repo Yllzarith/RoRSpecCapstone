683b8333e0486383d000b744f5223363f22b9709
Scott B <git@ibrogans.ca>
2017-05-30 23:49:17 -0400

    Scaffolded Foo object, branch up to Module 1 Example Requirements lecture

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526021140) do
+ActiveRecord::Schema.define(version: 20170531034229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "foos", force: :cascade do |t|
     t.string   "name"
